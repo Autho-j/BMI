25d14ca36c8eeaa1c8bf83aec24d2c5bbc2ffd85
Thor-Shan <71980949+Autho-j@users.noreply.github.com>
2021-09-26 09:37:51 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function calculate() {
+
+    //Getting user input
+    let height = document.getElementById("inputHeight").value
+    let weight = document.getElementById("inputWeight").value
+
+    let messageEl = document.getElementById("msgEl")
+    let sumEl = document.getElementById("sumEl")
+
+    //Calculating result
+    let heightToMetre = height / 100;
+    let heightTotal = heightToMetre * heightToMetre;
+    let weightTotal = weight / 2.205;
+    //formula
+    let sum = Math.floor(weightTotal / heightTotal)
+
+    //referencing messge
+
+
+    //Check the condition
+    if (sum < 18.5) {
+        sumEl.textContent = "BMI Score : " + sum
+        messageEl.textContent = "You are Underweight!"
+    } else if (sum >= 18.5 && sum <= 24.9) {
+        sumEl.textContent = "BMI Score : " + sum
+        messageEl.textContent = "You have Healthy Weight"
+    } else if (sum >= 25.0 && sum <= 29.9) {
+        sumEl.textContent = "BMI Score : " + sum
+        messageEl.textContent = "You are Overweight!"
+    } else if (sum > 30.0) {
+        sumEl.textContent = "BMI Score : " + sum
+        messageEl.textContent = "You are Obese!!"
+    }
+}
